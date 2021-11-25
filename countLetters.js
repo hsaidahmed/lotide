20c73b8d4173c0f4b93690593c2d837ecaf19d10
hsaidahmed <hashim.saidahmed@gmail.com>
2021-11-25 03:18:30 +0000

    count letters in a string

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const countLetters = function(string) {
+  const result = {};
+  for (let letter of string) {
+    if (letter !== " ") { // Filters the spaces in the string
+      if (result[letter]) { // If the letter has already been counted, adds 1 to the count
+        result[letter] += 1;
+      } else { // If the letter doesn't exist in the result object, creates it
+        result[letter] = 1;
+      }
+    }
+  }
+  return result;
+};
+console.log(countLetters("hashim"));
