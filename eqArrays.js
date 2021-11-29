1d6120be9209410c3636aa1d03a913f88df670f5
hsaidahmed <hashim.saidahmed@gmail.com>
2021-11-29 22:29:19 +0000

    commiting eqArrays refactored using mocha

--- eqArrays.js
+++ eqArrays.js
@@ -18,11 +18,12 @@ const eqArrays = (array1,array2) => {
   
   return true;
 };
+module.exports = eqArrays;
 
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
