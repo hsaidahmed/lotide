85dfdec266c7fcbd51bc048d48ee83eb3a7f9308
hsaidahmed <hashim.saidahmed@gmail.com>
2021-11-29 22:25:12 +0000

    commiting tailTest.js refactor for mocha

--- tail.js
+++ tail.js
@@ -14,13 +14,14 @@ const tail = function(array) {
   return x;
 };
 
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+// // const result = tail(["Hello", "Lighthouse", "Labs"]);
+// // assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
 
 
-const w = tail(words);
-assertEqual(w.length, 3);
+// const w = tail(words);
+// assertEqual(w.length, 3);
+module.exports = tail;
