661028de1a62a57d02b4317f5785fb981aa08f18
hsaidahmed <hashim.saidahmed@gmail.com>
2021-11-30 01:39:43 +0000

    Object updated in index.js

--- index.js
+++ index.js
@@ -17,27 +17,20 @@ const middle = require('./middle');
 
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertArraysEqual : assertArraysEqual,
-  assertEqual : assertEqual,
-  assertObjectsEqual : assertObjectsEqual,
-  countLetters : countLetters,
-  countOnly : countOnly,
-  eqArrays : eqArrays,
-  eqObjects : eqObjects,
-  findKey : findKey,
-  findKeyByValue : findKeyByValue,
-  letterPositions : letterPositions,
-  map : map,
-  takeUntil : takeUntil,
-  without : without
-
-
-
-
-
-
-
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 };
